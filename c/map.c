1b53636934167b58050924c64fd1ae36ae357843
Utkarsh Gupta <guptautkarsh2102@gmail.com>
2018-07-15 20:41:57 +0530

    [Added] C Map Program.
    Signed-off-by: Utkarsh Gupta <guptautkarsh2102@gmail.com>

---
+++ c/map.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+main()
+{
+	int a,b,c;
+	int count = 1;
+	for (b=c=10;a=”- FIGURE?, UMKC,XYZHello Folks,TFy!QJu ROo TNn(ROo)SLq SLq ULo+UHs UJq TNn*RPn/QPbEWS_JSWQAIJO^ NBELPeHBFHT}TnALVlBLOFAkHFOuFETpHCStHAUFAgcEAelclcn^r^r\tZvYxXyT|S~Pn SPm SOn TNn ULo0ULo#ULo-WHq!WFs XDt!” [b+++21]; )
+	for(; a– > 64 ; )
+	putchar ( +c==’Z’ ? c = c/ 9:33^b&1);
+	return 0;
+}
